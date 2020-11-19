621d9c7e2545ae1900b2fb46c1c2d0b01d3e0a0a
kentmonh <mnguyen2606@gmail.com>
2020-11-19 12:54:00 +0700

    Seed data to product table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+require "faker"
+
+# Clearing our data
+Product.delete_all
+Brand.delete_all
+
+# Loop through the rows of a CSV file
+csv_file = Rails.root.join("db/perfume.csv")
+
+csv_data = File.read(csv_file)
+
+perfumes = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+perfumes.each do |perfume|
+  brand = Brand.find_or_create_by(name: perfume["Brand"])
+
+  p = brand.products.create(
+    sku:         Faker::Barcode.unique.upc_a,
+    name:        perfume["Name"],
+    price:       Faker::Commerce.price(range: 30..100.0),
+    sale_price:  Faker::Commerce.price(range: 30..100.0),
+    stock:       Faker::Number.within(range: 5..500),
+    description: Faker::Marketing.buzzwords
+  )
+
+  puts "Invalid perfume #{perfume['Name']}" unless p&.valid?
+end
+
+puts "Created #{Brand.count} brands"
+puts "Created #{Product.count} products"
