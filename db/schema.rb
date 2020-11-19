14663d48ffa3cb2b2ca0da6d94b2405eddc4c35b
kentmonh <mnguyen2606@gmail.com>
2020-11-19 23:00:37 +0700

    Add views to Page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_082843) do
+ActiveRecord::Schema.define(version: 2020_11_19_154837) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -67,6 +67,14 @@ ActiveRecord::Schema.define(version: 2020_11_19_082843) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
