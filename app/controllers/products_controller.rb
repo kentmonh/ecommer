dc34194287302cfa32393ce35ce86fd06dbe4d80
kentmonh <mnguyen2606@gmail.com>
2020-11-19 19:00:44 +0700

    Added product controller and views.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
-  def view
-  end
+  def view; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/view'
-  get 'products/index'
-  get 'products/show'
+  resources :products, only: %i[index show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
