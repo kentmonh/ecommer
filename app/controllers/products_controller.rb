cb09cdfeb53a8f336288f97cf5026aa7059b009a
kentmonh <mnguyen2606@gmail.com>
2020-11-20 16:20:56 +0700

    Set filter for index of products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,4 +14,18 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def filter
+    filter = params[:filter_product]
+    if filter == "Sale Products"
+      @filter_keyword = "Sale Products"
+      @filter_products = Product.where("sale_price < price")
+    elsif filter == "New Products"
+      @filter_keyword = "New Products"
+      @filter_products = Product.where("created_at >= ?", Time.zone.today - 3.days)
+    else
+      @filter_keyword = "All Products"
+      @filter_products = Product.all
+    end
+  end
 end
