746b915fc6e09f8c88539d34a441b6b17bc57a12
kentmonh <mnguyen2606@gmail.com>
2020-11-27 15:47:01 +0700

    Fix conflict.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -24,29 +24,3 @@ class CartController < ApplicationController
   # Show the session cart
   def index; end
 end
-class CartController < ApplicationController
-  # POST /cart
-  def create
-    id = params[:id].to_i
-
-    unless session[:cart].include?(id)
-      session[:cart] << id
-      session[:quantity] << 1
-      product = Product.find(id)
-      flash[:add_cart] = "#{product.name} added to the Cart."
-      redirect_to root_path
-    end
-  end
-
-  # DELETE /cart/:id
-  def destroy
-    id = params[:id].to_i
-    session[:cart].delete(id)
-    product = Product.find(id)
-    flash[:remove_cart] = "#{product.name} removed from the Cart."
-    redirect_to cart_index_path
-  end
-
-  # Show the session cart
-  def index; end
-end
