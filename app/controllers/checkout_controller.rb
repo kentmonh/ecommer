0a49e32bed7e8a3a199c12656b62d2c097fffa90
kentmonh <mnguyen2606@gmail.com>
2020-12-03 06:32:49 +0700

    Change some style.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -10,9 +10,11 @@ class CheckoutController < ApplicationController
     new_full_name = params[:full_name].to_s
     new_phone = params[:phone].to_s
     new_address = params[:address].to_s
+    new_province = params[:select_province]
     current_user.full_name = new_full_name
     current_user.phone = new_phone
     current_user.address = new_address
+    current_user.province_id = new_province
     current_user.save
     redirect_to checkout_index_path
   end
